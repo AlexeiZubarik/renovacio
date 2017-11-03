d1bb42e402ee93d9ce3da35643236bc56b277f3f
Alexei Zubarik <alesha.zubarik@yandex.ru>
2017-11-03 18:35:54 +0300

    fix

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('#img1').click(function() {
+  $('#text1-show').slideToggle(function() {
+    $('#text1-show p').show('slow');
+  });
+}); 
+
+$('#img2').click(function() {
+  $('#text1-show').slideToggle(function() {
+    $('#text2-show p').show('slow');
+  });
+}); 
+
+$('#img3').click(function() {
+  $('#text1-show').slideToggle(function() {
+    $('#text3-show p').show('slow');
+  });
+}); 
