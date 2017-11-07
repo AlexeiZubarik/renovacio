b4b72ed9278761ce4780100c856831e5953c93c4
AlexeiZubarik <alesha.zubarik@yandex.ru>
2017-11-07 11:37:18 +0300

    add sleder

--- js/script.js
+++ js/script.js
@@ -5,13 +5,24 @@ $('#img1').click(function() {
 }); 
 
 $('#img2').click(function() {
-  $('#text1-show').slideToggle(function() {
+  $('#text2-show').slideToggle(function() {
     $('#text2-show p').show('slow');
   });
 }); 
 
 $('#img3').click(function() {
-  $('#text1-show').slideToggle(function() {
+  $('#text3-show').slideToggle(function() {
     $('#text3-show p').show('slow');
   });
-}); 
+});
+
+var autoplaySlider = $('#autoplay').lightSlider({
+  item: 4,
+  auto: true,
+  loop: true,    
+  pauseOnHover: true,
+  onBeforeSlide: function (el) {
+    $('#current').text(el.getCurrentSlideCount());
+  } 
+});
+$('#total').text(autoplaySlider.getTotalSlideCount());
