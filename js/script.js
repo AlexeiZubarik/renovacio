92681dcccf2688d88c7655cb668e2a0b05ca61ff
Alexei Zubarik <alesha.zubarik@yandex.ru>
2017-11-13 11:17:04 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,32 +1,74 @@
 $('#img1').click(function() {
   $('#text1-show').slideToggle(function() {
+    $('html, body').animate({ scrollTop: $('#text1-show').offset().top - 100 }, 'slow');
   });
 }); 
 
 $('#img2').click(function() {
   $('#text2-show').slideToggle(function() {
+    $('html, body').animate({ scrollTop: $('#text2-show').offset().top - 100 }, 'slow');
   });
 }); 
 
 $('#img3').click(function() {
   $('#text3-show').slideToggle(function() {
+    $('html, body').animate({ scrollTop: $('#text3-show').offset().top - 100 }, 'slow');
   });
 });
 
 $('#img4').click(function() {
   $('#text4-show').slideToggle(function() {
+    $('html, body').animate({ scrollTop: $('#text4-show').offset().top - 100 }, 'slow');
   });
 }); 
 
 $('#img5').click(function() {
   $('#text5-show').slideToggle(function() {
+    $('html, body').animate({ scrollTop: $('#text5-show').offset().top - 100 }, 'slow');
   });
 }); 
 
 $('#img6').click(function() {
   $('#text6-show').slideToggle(function() {
+    $('html, body').animate({ scrollTop: $('#text6-show').offset().top - 100 }, 'slow');
   });
 });
+
+$('#btn1').click(function() {
+  $('#text1-show').slideToggle(function() {
+    $('html, body').animate({ scrollTop: $('#img1').offset().top - 300}, 'slow');
+  });
+})
+
+$('#btn2').click(function() {
+  $('#text2-show').slideToggle(function() {
+    $('html, body').animate({ scrollTop: $('#img2').offset().top - 300}, 'slow');
+  });
+})
+
+$('#btn3').click(function() {
+  $('#text3-show').slideToggle(function() {
+    $('html, body').animate({ scrollTop: $('#img3').offset().top - 300}, 'slow');
+  });
+})
+
+$('#btn4').click(function() {
+  $('#text4-show').slideToggle(function() {
+    $('html, body').animate({ scrollTop: $('#img4').offset().top - 300}, 'slow');
+  });
+})
+
+$('#btn5').click(function() {
+  $('#text5-show').slideToggle(function() {
+    $('html, body').animate({ scrollTop: $('#img5').offset().top - 300}, 'slow');
+  });
+})
+
+$('#btn6').click(function() {
+  $('#text6-show').slideToggle(function() {
+    $('html, body').animate({ scrollTop: $('#img6').offset().top - 300}, 'slow');
+  });
+})
 
 var icons = {
   header: "glyphicon glyphicon-plus-sign",
